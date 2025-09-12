040ee5b69864934375a63402ac3192b43e49f244
Lauryn Sylvain <lsylvain@broncos.uncfsu.edu>
2025-09-12 11:27:41 -0400

    Put main.py in saved changes

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = "Escaping"
+print("Welcome to", game_name + "!")  # TODO, print greetings with game name
+print("====================") # TODO, add or remove equal symbol to align the greetings
+
+# Ask for the character's name
+name = input("Before we begin, what is your character's name?\n> ")
+
+# Print the name
+print("Great, " + name + "!", "Let's begin the adventure!")
